557d3ab155fa2fdd6b81cd2b9b474478ee4e12d4
Gabriel Porto <gr_porto@hotmail.com>
2024-01-05 17:42:26 -0300

    `class:` domain modeling with domain-driven design

---
+++ src/Domain/Users/User.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Abstractions;
+
+namespace Domain.Users;
+
+public sealed class User : Entity
+{
+    private User(Guid id, Name name)
+        : base (id)
+    {
+        Name = name;
+    }
+
+    public Name Name { get; private set; }
+
+    public static User Create(Name name)
+    {
+        var user = new User(Guid.NewGuid(), name);
+
+        user.Raise(new UserCreatedDomainEvent(user.Id));
+
+        return user;
+    }
+}
---
+++ tests/Domain.UnitTests/Users/UserTests.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Users;
+using FluentAssertions;
+
+namespace Domain.UnitTests.Users;
+
+public class UserTests
+{
+    [Fact]
+    public void Create_Should_ReturnUser_WhenNameIsValid()
+    {
+        // Arrange
+        var name = new Name("Full Name");
+
+        // Act
+        var user = User.Create(name);
+
+        // Assert
+        user.Should().NotBeNull();
+    }
+
+    [Fact]
+    public void Create_Should_RaiseDomainEvent_WhenNameIsValid()
+    {
+        // Arrange
+        var name = new Name("Full Name");
+
+        // Act
+        var user = User.Create(name);
+
+        // Assert
+        user.DomainEvents.Should().ContainSingle()
+            .Which.Should().BeOfType<UserCreatedDomainEvent>();
+    }
+}
