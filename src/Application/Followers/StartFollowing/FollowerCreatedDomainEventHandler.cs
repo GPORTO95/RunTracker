e877809340a16a6dff63edb83d178461a5b8212a
Gabriel Porto <gr_porto@hotmail.com>
2024-01-26 17:48:24 -0300

    `class:` How To Make Your Application Queries Reusable

--- src/Application/Followers/StartFollowing/FollowerCreatedDomainEventHandler.cs
+++ src/Application/Followers/StartFollowing/FollowerCreatedDomainEventHandler.cs
@@ -1,4 +1,7 @@
-﻿using Application.Abstractions.Notifications;
+﻿using System.Data;
+using Application.Abstractions.Data;
+using Application.Abstractions.Notifications;
+using Application.Users;
 using Domain.Followers;
 using MediatR;
 
@@ -8,17 +11,32 @@ internal sealed class FollowerCreatedDomainEventHandler
     : INotificationHandler<FollowerCreatedDomainEvent>
 {
     private readonly INotificationService _notificationService;
+    private readonly IApplicationDbContext _dbContext;
+    private readonly IDbConnectionFactory _dbConnectionFactory;
 
-    public FollowerCreatedDomainEventHandler(INotificationService notificationService)
+    public FollowerCreatedDomainEventHandler(
+        INotificationService notificationService,
+        IApplicationDbContext dbContext,
+        IDbConnectionFactory dbConnectionFactory)
     {
         _notificationService = notificationService;
+        _dbContext = dbContext;
+        _dbConnectionFactory = dbConnectionFactory;
     }
 
     public async Task Handle(FollowerCreatedDomainEvent notification, CancellationToken cancellationToken)
     {
+        // STEP 1: Dapper
+        using IDbConnection connection = _dbConnectionFactory.CreateOpenConnection();
+
+        UserDto user = await UserQueries.GetByIdAsync(notification.UserId, connection);
+
+        // STEP 2: EF Core
+        // UserDto user = await _dbContext.GetByIdAsyncEf(notification.UserId, cancellationToken);
+
         await _notificationService.SendAsync(
             notification.FollowedId,
-            "You just got a new follower",
+            $"{user.Name} just got a new follower",
             cancellationToken);
     }
 }
---
+++ src/Application/Users/UserQueries.cs
@@ -0,0 +1,33 @@
+﻿using System.Data;
+using Application.Abstractions.Data;
+using Dapper;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Users;
+
+public static class UserQueries
+{
+    public static async Task<UserDto> GetByIdAsync(
+        Guid id,
+        IDbConnection connection)
+    {
+        return await connection.QuerySingleAsync<UserDto>(
+            """
+            SELECT Id, Name
+            FROM Users
+            WHERE Id = @UserId
+            """,
+            new { UserId = id});
+    }
+
+    public static async Task<UserDto> GetByIdAsyncEf(
+        this IApplicationDbContext dbContext,
+        Guid id,
+        CancellationToken cancellationToken)
+    {
+        return await dbContext.Users
+            .Where(u => u.Id == id)
+            .Select(u => new UserDto(u.Id, u.Name.Value))
+            .SingleAsync(cancellationToken);
+    }
+}
