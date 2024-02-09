61b800be28f3cea8b5ed104ade2a472d24a7ff33
Gabriel Porto <gr_porto@hotmail.com>
2024-02-08 22:50:01 -0300

    feat: add match result

--- src/Web.Api/Extensions/ResultExtensions.cs
+++ src/Web.Api/Extensions/ResultExtensions.cs
@@ -5,6 +5,22 @@ namespace Web.Api.Extensions;
 
 public static class ResultExtensions
 {
+    public static TOut Match<TOut>(
+        this Result result,
+        Func<TOut> onSuccess,
+        Func<Result, TOut> onFailure)
+    {
+        return result.IsSuccess ? onSuccess() : onFailure(result); 
+    }
+
+    public static TOut Match<TIn, TOut>(
+        this Result<TIn> result,
+        Func<TIn, TOut> onSucces,
+        Func<Result<TIn>, TOut> onFailure)
+    {
+        return result.IsSuccess ? onSucces(result.Value) : onFailure(result);
+    }
+
     public static IResult ToProblemDetails(this Result result)
     {
         if (result.IsSuccess)
