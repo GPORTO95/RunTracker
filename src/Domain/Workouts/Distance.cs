52c4a7ee86527f48cb5421791e4f393c220cd720
Gabriel Porto <gr_porto@hotmail.com>
2024-02-14 23:04:01 -0300

    add class Distance

---
+++ src/Domain/Workouts/Distance.cs
@@ -0,0 +1,24 @@
+﻿using SharedKernel;
+
+namespace Domain.Workouts;
+
+public sealed class Distance
+{
+    private const decimal OneKilometer = 1_000.0m;
+
+    public Distance(decimal meters)
+    {
+        Ensure.GreaterThanZero(meters);
+
+        Meters = meters;
+    }
+
+    private Distance()
+    {
+        
+    }
+
+    public decimal Meters { get; init; }
+
+    public decimal Kilometers => Meters / OneKilometer;
+}
