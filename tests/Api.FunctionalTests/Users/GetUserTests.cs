78e53e119eb95fa7d7ec0c952909ff2049a8010e
Gabriel Porto <gr_porto@hotmail.com>
2024-02-18 20:01:42 -0300

    test: add test when user does not exists

---
+++ tests/Api.FunctionalTests/Users/GetUserTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using Api.FunctionalTests.Abstractions;
+using FluentAssertions;
+
+namespace Api.FunctionalTests.Users;
+
+public class GetUserTests : BaseFunctionalTest
+{
+    public GetUserTests(FunctionalTestWebAppFactory factory)
+        : base(factory)
+    {
+    }
+
+    [Fact]
+    public async Task Should_ReturnNotFound_WhenUserDoesNotExist()
+    {
+        // Arrange
+        var userId = Guid.NewGuid();
+
+        // Act
+        HttpResponseMessage response = await HttpClient.GetAsync($"api/users/{userId}");
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+    }
+}
