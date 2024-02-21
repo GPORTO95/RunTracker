aca8a66c33fd3c83717d872a8e8c70b58deaf88c
Gabriel Porto <gr_porto@hotmail.com>
2024-02-20 21:37:48 -0300

    feat: add command for create workout

---
+++ src/Application/Workouts/Create/CreateWorkoutCommand.cs
@@ -0,0 +1,5 @@
+﻿using Application.Abstractions.Messaging;
+
+namespace Application.Workouts.Create;
+
+public sealed record CreateWorkoutRequest(Guid UserId, string Name);
---
+++ src/Application/Workouts/WorkoutErrorCodes.cs
@@ -0,0 +1,11 @@
+﻿namespace Application.Workouts;
+
+public static class WorkoutErrorCodes
+{
+    public static class Create
+    {
+        public const string MissingUserId = nameof(MissingUserId);
+        public const string MissingName = nameof(MissingName);
+        public const string NameTooLong = nameof(NameTooLong);
+    }
+}
